d3ee482c7f6fac0caa8ad7dd6e072aa6d3ac28f3
Pandananana <andreaswendelboe.dk@gmail.com>
2023-06-06 12:39:33 +0200

    Fast PWM with custom top

--- Code/PID Controller/lib/Timer/timer.c
+++ Code/PID Controller/lib/Timer/timer.c
@@ -33,10 +33,11 @@ void init_pc_pwm() {
 
 void init_fast_pwm() {
     // Set data direction on pinb6 to output
-	DDRB |= (1<<PB5);
-    // Set Wave Gen Mode to Fast PWM
-	TCCR1A |= (1<<WGM12)|(1<<WGM11)|(1<<WGM10)|(1<<COM1A1);
-	// Set prescaling to 1
-	TCCR1B |= (1<<CS10);
+    DDRB |= (1<<PB6);
+    // Set Wave Gen Mode to Fast PWM with OCR1A top
+    TCCR1A = 1<<COM1B1 | 1<<WGM11 | 1<<WGM10;
+	TCCR1B = 1<<WGM13 | 1<<WGM12 | 1<<CS10;
+	// 160 kHz at 16Mhz
+	OCR1A = 100;
 }
 
--- Code/PID Controller/src/main.c
+++ Code/PID Controller/src/main.c
@@ -14,7 +14,6 @@
 void init()
 {
     init_fast_pwm();
-    OCR1A = 0;
 
     // ADC set uo
     ADC_init();
@@ -31,44 +30,7 @@ int main(void)
     init();
     sei();
 
-    char send[6];
-
-    float uvals = 0, yvals = 0;
-    float y[3] = {0.0,0.0,0.0};
-    float u[3] = {0.0,0.0,0.0};
-
-    OCR1A = 300;
-    _delay_ms(2000);
-
-    int ref = 512;
-
-    while (1)
-    {
-        if (flag_ADC==1)
-        {
-            // Find error
-            u[0] = ref - ADC_new;
-
-            // Calculate output
-            uvals = 10.49 * u[0] - 20.96 * u[1] + 10.47 * u[2];
-            yvals = 1.993 * y[1] - 0.9928 * y[2];
-            y[0] = uvals + yvals;
-            // dtostrf((int)y[0], 4, 2, send);
-            // sprintf(send, "%s\n", &send);
-            // putsUART0(send);
-            OCR1A = (int)y[0];
-
-            // Cycling outputs
-            y[2] = y[1];
-            y[1] = y[0];
-
-            // Cycling inputs
-            u[2] = u[1];
-            u[1] = u[0];
-
-            flag_ADC=0;
-        }   
-    }
+    OCR1B = 90;
 }
 
 
