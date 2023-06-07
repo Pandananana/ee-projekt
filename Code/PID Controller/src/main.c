dfa58a8c31aea0d7c97ab251a218c8d2cb95b910
fromafl <fromafl@gmail.com>
2023-06-07 14:28:44 +0200

    Basic PID

--- Code/PID Controller/src/main.c
+++ Code/PID Controller/src/main.c
@@ -20,7 +20,7 @@ void init()
 
     // Uart set up
     uart0_Init(MYUBBRF);
-    char send[32];
+    //char send[32];
     // sprintf(send,"init");
     // putsUART0(send);
 }
@@ -30,10 +30,10 @@ int main(void)
     init();
     sei();
 
-    char send[20];
-    uint16_t terminal[1000], test_count = 0;
+    char send[32];
+    //uint16_t terminal[1000], test_count = 0;
 
-    double uvals = 0, yvals = 0;
+    /*double uvals = 0, yvals = 0;
     double y[3] = {0.0, 0.0, 0.0};
     double u[3] = {0.0, 0.0, 0.0};
 
@@ -48,41 +48,81 @@ int main(void)
         -1,
         0
     };
-
-    OCR1B = 71;
+    */
+    OCR1B = 1;
     _delay_ms(5000);
     DDRB |= (1<<PB4);
 
-    uint16_t ref = 500;
+    float iMax = 1000;
+    float iMin = -1000;
+    int Err_Value;
+    float P_Term;
+    float I_Term;
+    float D_Term;
+    int new_ADC_value;
+    float PWM_Duty;
+    float Kp = 0.139,Ki = 0.2, Kd = 0;
+    float d_Temp = 0;
+    float i_Temp = 0;
+    uint16_t PWM_Temp= 10;
+    uint16_t ref = 512;
 
     while (1)
     {
         if (flag_ADC == 1)
         {
-            PORTB ^= (1<<PB4);
-            // Find error
-            u[0] = ref - ADC_new;
+            //new_ADC_value = ADC_new;
+            Err_Value = (ref - ADC_new);
+            P_Term = Kp * Err_Value;
+            
+            
+            i_Temp = i_Temp + Err_Value;
+            if (i_Temp > iMax){
+                i_Temp = iMax;
+                }
+                else if (i_Temp < iMin){
+                    i_Temp = iMin;
+                }
+            I_Term = Ki * i_Temp;
+            PWM_Temp =  (int)(P_Term + I_Term);
+             if (PWM_Temp < 1){
+                PWM_Temp = 0;
+            }
+            sprintf(send, "%i,%i,%i\n", PWM_Temp,(int)Err_Value,(int)i_Temp);
+            putsUART0(send);
+            OCR1B = PWM_Temp;
 
-            // Calculate output
+
+
+
+
+
+
+            flag_ADC = 0;
+           // PORTB ^= (1<<PB4);
+            // Find error
+            //u[0] = (ref - ADC_new)*0.139;
+            //OCR1B = (int)u[0];
+           /* // Calculate output
             uvals = num[0] * u[0] + num[1] * u[1] + num[2] * u[2];
             yvals = -den[1] * y[1] - den[2] * y[2];
             y[0] = uvals + yvals;
             
             OCR1B = (int)y[0];
-            
+            */
             // dtostrf(y[0], 4, 2, send);
             // sprintf(send, "%d\n", ADC_new);
             // putsUART0(send);
 
             // Cycling outputs
-            y[2] = y[1];
+           /* y[2] = y[1];
             y[1] = y[0];
 
             // Cycling inputs
             u[2] = u[1];
-            u[1] = u[0];
+            u[1] = u[0];*/
 
-            flag_ADC = 0;
+            //flag_ADC = 0;
 
             // if ((test_count > 999))
             // {
