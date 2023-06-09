9287340a57f85b8d4f1926dfbaa821310ae33c88
Magnus <maggi5621@gmail.com>
2023-06-09 16:33:41 +0200

    BuckPID Code meget

--- Code/Buck converter PWM/src/main.cpp
+++ Code/Buck converter PWM/src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
+#include <Controler.h>
 
 const int pwmPin = 10; // PWM output pin
 const int trigger = 8;
+const int analogPin = A1;   // Analog pin to read from
+volatile int analogValue;   // Volatile variable to store the analog value
+const unsigned long samplingInterval = 100;  // Sampling interval in microseconds
+
 
 void setup()
 {
@@ -23,19 +28,38 @@ void setup()
     TCCR1B |= (1 << CS10);
 
     // Set top count value (determines the PWM frequency)
-    ICR1 = 319;
+    ICR1 = 159;
+
+    DDRB |= (1<<PB0);
 
     // Set the PWM duty cycle by writing to OCR1A or OCR1B (value must be less than ICR1)
-    OCR1B = 160;
+    OCR1B = 80;
+    Serial.begin(9600);  // Initialize serial communication
+    analogReference(DEFAULT);  // Set the default analog reference voltage (5V)
+
+    ADCSRA = (1<<ADPS2) | (1<<ADPS1);
+
+    // Configure Timer1 to generate an interrupt at the desired sampling rate
+    cli();  // Disable interrupts
+    TCCR0A = 0;  // Clear Timer0 control registers
+    TCCR0B = 0;
+    TCNT0 = 0;  // Initialize Timer0 counter value
+    OCR0A = 199;  // Set the compare match value for 10 kHz sampling rate
+    TCCR0A |= (1 << WGM01);  // Configure Timer0 for CTC mode
+    TCCR0B |= (1 << CS01);  // Set prescaler to 64
+    TIMSK0 |= (1 << OCIE0A);  // Enable Timer0 compare match interrupt
+    sei();  // Enable interrupts
 }
 
-void loop()
-{
-  OCR1B = 111;
-  digitalWrite(trigger, 0);
-  delay(5000);
+void loop() {
+  // Print the analog value
+  Serial.print("Analog Value: ");
+  Serial.println(analogValue);
+  delay(1000);
+}
 
-  OCR1B = 143;
-  digitalWrite(trigger, 1);
-  delay(10000);
+// Interrupt Service Routine (ISR) for Timer1 compare match
+ISR(TIMER0_COMPA_vect) {
+  analogValue = analogRead(analogPin);  // Read analog value
+  PORTB ^= (1<<PB0);
 }
