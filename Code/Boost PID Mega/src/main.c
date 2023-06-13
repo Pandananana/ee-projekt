174b4b43a4698f7818e2063267b574a2b3be9bc5
Pandananana <andreaswendelboe.dk@gmail.com>
2023-06-13 12:53:38 +0200

    Boost control working - non PID

--- Code/Boost PID Mega/src/main.c
+++ Code/Boost PID Mega/src/main.c
@@ -19,8 +19,6 @@ void init()
     // 100 kHz at 16Mhz
     OCR1A = PWM_TOP;
 
-    uart0_Init(MYUBBRF);
-
     // ADC set up
     ADC_init();
 }
@@ -30,48 +28,37 @@ int main(void)
     init();
     sei();
 
-    float ref = 512.0;
+    float ref = 405.0;
 
     int OCR1B_signed = (PWM_TOP - 1);
-
-    char send[32];
-    uint16_t test_count = 0;
 
     OCR1B = OCR1B_signed;
 
     while (1)
     {
-        OCR1B = 50;
-        // _delay_ms(1);
-        // if (ADC_new > ref)
-        // {
-        //     OCR1B_signed++;
-        // }
-        // else if (ADC_new < ref)
-        // {
-        //     OCR1B_signed--;
-        // }
+        _delay_ms(10);
+        if (ADC_new > ref)
+        {
+            OCR1B_signed--;
+        }
+        else if (ADC_new < ref)
+        {
+            OCR1B_signed++;
+        }
 
-        // if (OCR1B_signed > PWM_TOP)
-        // {
-        //     OCR1B_signed = PWM_TOP;
-        // }
-        // else if (OCR1B_signed < 0)
-        // {
-        //     OCR1B_signed = 0;
-        //     DDRB &= (0 << PB6);
-        // }
-        // else
-        // {
-        //     DDRB |= (1 << PB6);
-        // }
-        // OCR1B = OCR1B_signed;
-        // test_count++;
-        // if (test_count >= 1000)
-        // {
-        //     sprintf(send, "%d\n", ADC_new);
-        //     putsUART0(send);
-        //     test_count = 0;
-        // }
+        if (OCR1B_signed > 80)
+        {
+            OCR1B_signed = 80;
+        }
+        else if (OCR1B_signed < 0)
+        {
+            OCR1B_signed = 0;
+            DDRB &= (0 << PB6);
+        }
+        else
+        {
+            DDRB |= (1 << PB6);
+        }
+        OCR1B = OCR1B_signed;
     }
 }
